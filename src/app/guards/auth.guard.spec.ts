9ebe1779cda2b5ed28c67df44fd905752c0ec01a
Szamosvári Márk <xuumj7@inf.elte.hu>
2025-05-05 19:31:06 +0200

    feat: Tests added to check onCardClicked function

--- src/app/guards/auth.guard.spec.ts
+++ src/app/guards/auth.guard.spec.ts
@@ -1,11 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 import { CanActivateFn } from '@angular/router';
 
-import { authGuard } from './auth.guard';
+import { AuthGuard } from './auth.guard';
 
 describe('authGuard', () => {
   const executeGuard: CanActivateFn = (...guardParameters) => 
-      TestBed.runInInjectionContext(() => authGuard(...guardParameters));
+      TestBed.runInInjectionContext(() => {
+        const mockRouter = jasmine.createSpyObj('Router', ['navigate']);
+        const authGuard = new AuthGuard(mockRouter);
+        return authGuard.canActivate();
+      });
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
