70251e555484e26310baa348d1a92c9c98532129
Szamosvári Márk <xuumj7@inf.elte.hu>
2025-05-06 17:07:03 +0200

    fix: remove unused import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { GameCardComponent } from './components/game-card/game-card.component';
-import { GameService } from './services/game.service';
+
 
 
 
