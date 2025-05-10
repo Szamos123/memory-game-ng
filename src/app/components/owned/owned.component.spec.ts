327ab660cb0bfcdd98840be9ba5c123ed420d9be
Szamosvári Márk <xuumj7@inf.elte.hu>
2025-05-10 20:22:58 +0200

    Feat: Owned component, users can see the skins they have + tests

---
+++ src/app/components/owned/owned.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { OwnedComponent } from './owned.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+describe('OwnedComponent', () => {
+  let component: OwnedComponent;
+  let fixture: ComponentFixture<OwnedComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [OwnedComponent, HttpClientTestingModule]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(OwnedComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
