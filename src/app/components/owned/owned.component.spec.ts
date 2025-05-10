7515d00733fd1766599be4f4bd3d933de4bd0827
Szamosvári Márk <xuumj7@inf.elte.hu>
2025-05-10 21:06:32 +0200

    fixed

--- src/app/components/owned/owned.component.spec.ts
+++ src/app/components/owned/owned.component.spec.ts
@@ -1,7 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { OwnedComponent } from './owned.component';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';  // To mock the observable returned by ActivatedRoute
 
 describe('OwnedComponent', () => {
   let component: OwnedComponent;
@@ -9,7 +10,13 @@ describe('OwnedComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [OwnedComponent, HttpClientTestingModule],
+      imports: [HttpClientTestingModule, OwnedComponent], 
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: { snapshot: { paramMap: of({}) } } 
+        }
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(OwnedComponent);
